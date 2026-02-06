bede4b5009311d2ac8edf0f4076c333ed09b4309
Shayan banik <shayan@Shayans-MacBook-Air.local>
2026-02-06 12:18:44 +0530

    setup databse docker prisma7

---
+++ src/lib/db.js
@@ -0,0 +1,19 @@
+import { PrismaClient } from "../app/generated/prisma/client";
+import { PrismaPg } from "@prisma/adapter-pg";
+
+const globalForPrisma = globalThis;
+
+const adapter = new PrismaPg({
+  connectionString: process.env.DATABASE_URL,
+});
+
+const db =
+  globalForPrisma.prisma ||
+  new PrismaClient({
+    adapter,
+    log: ["query", "info", "warn", "error"],
+  });
+
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = db;
+
+export default db;
